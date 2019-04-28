9221332a686809b3ccc086d628cbb07b623f737e
MeniscusLight <Mr_JWYYX@163.com>
2019-04-28 08:48:06 +0800

    new defs file

--- RPGDemo/Assets/Scripts/GameGlobalCenter.cs
+++ RPGDemo/Assets/Scripts/GameGlobalCenter.cs
@@ -1,16 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
 
 
 public class GameGlobalCenter : MonoBehaviour
 {
-
+    private uint m_prevUpdateLogicTickerTime = 0;
     private uint m_globalUpdateFrame = 0;
+
+    private Stopwatch m_stopWatch = null;
 
 	void Start ()
     {
-		
+        m_stopWatch = new Stopwatch();
+        m_stopWatch.Start();
 	}
 	
 	void Update ()
---
+++ RPGDemo/Assets/Scripts/GlobalDefs/GameLogicDefs.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameLogicDefs
+{
+    public static uint GAME_LOGIC_FRAME_RATE = 15;
+    public static uint GAME_VIEW_FRAME_RATE = 30;
+}
