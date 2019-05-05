1fe5ea1e8dfeee475aeba69ee1bf269d7e56520a
MeniscusLight <Mr_JWYYX@163.com>
2019-05-05 22:39:19 +0800

    modify logicData

---
+++ RPGDemo/Assets/Scripts/GameLogic/LogicDatas/GameLogicDefs.cs
@@ -0,0 +1,11 @@
+﻿
+public class GameLogicDefs
+{
+    public const uint GAME_LOGIC_FRAME_RATE = 15;
+    public const uint GAME_VIEW_FRAME_RATE = 30;
+
+    public const uint GAME_TICKER_FULL_SCALE = 100;
+    public const uint TICKER_GROUP_UPDATE_MAX_COUNT = 5;
+    public const uint LOGIC_TICK_MAX_DISTANCE = 20;
+
+}
