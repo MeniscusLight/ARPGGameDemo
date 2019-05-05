e52843d6092eb9eac10f81e59d9a16bab6b2d5bd
MeniscusLight <Mr_JWYYX@163.com>
2019-05-05 23:16:57 +0800

    add logicScene, logicManagerCenter

---
+++ RPGDemo/Assets/Scripts/GameLogic/LogicManagers/LogicManagerCenter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GameLogic
+{
+    public class LogicManagerCenter
+    {
+        private TickerManager m_testTickerMgr = null;
+
+        public LogicManagerCenter()
+        {
+
+        }
+
+        public void Init()
+        {
+
+        }
+    }
+}
---
+++ RPGDemo/Assets/Scripts/GameLogic/LogicScene.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GameLogic
+{
+    public class LogicScene
+    {
+        public LogicScene()
+        {
+
+        }
+
+        public void Init()
+        {
+
+        }
+
+    }
+}
